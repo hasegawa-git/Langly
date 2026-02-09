d5ac82ca6c63c58a2b7924b226aeaf8898dd70e8
hasegawa-git <takahiro.hasegawa.biz@gmail.com>
2026-02-09 21:14:21 +0900

    feat: implement login logic with bcrypt and database query

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { query } from "@/lib/db"
+import { query } from "@/lib/db";
+import bcrypt from "bcrypt";
+import { use } from "react";
 
 // 認証を行う関数を定義
 const handler = NextAuth({
@@ -12,10 +14,40 @@ const handler = NextAuth({
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" }
       },
+
+      // 入力されたメールアドレスとパスワードが正しいかをDBに問い合わせる
       async authorize(credentials) {
-        // 入力されたメールアドレスとパスワードが正しいかをDBに問い合わせる
-        console.log("Login attempt:", credentials?.email);
-        return null
+        if (!credentials?.email || !credentials?.password) return null;
+
+        // DBからユーザーを探す
+        const result = await query(
+          "SELECT * FROM users WHERE email = $1",
+          [credentials.email]
+        );
+        const user = result.rows[0];
+
+        // ユーザーが存在しない場合はNG
+        if (!user) {
+          console.log("User not found");
+          return null;
+        }
+
+        // パスワードを照合する
+        const isPasswordCorrect = await bcrypt.compare(
+          credentials.password,
+          user.password_hash
+        );
+        // 失敗した場合、エラーメッセージを残す
+        if (!isPasswordCorrect) {
+          console.log("Invalid password");
+          return null;
+        }
+        // 成功した場合、ユーザー情報を返す
+        return {
+          id: user.id.toString(),
+          email: user.email,
+          name: user.email.split('@')[0],
+        };
       }
     })
   ],
@@ -39,6 +71,8 @@ export { handler as GET, handler as POST };
 - GoogleProvider: Googleアカウントでログイン
 - GithubProvider: GitHubアカウントでログイン
 - authorize(): 本人確認の合否を決める関数でログインをクリックすると呼び出され、ログインフォームに入力された値が引数に渡る
+- bcrypt.compare(): 第一引数と第二引数が一致するかを調べるbcryptのメソッド
+- query(): lib/db.tsで定義した自作関数。第1引数にSQL、第2引数にデータを受け取る。
 - session:{} : ログイン状態(セッション)の設定を記述するオブジェクト
 - strategy: "jwt": jwtでセッション管理する
 - strategy: "database": データベースでセッション情報を管理する
